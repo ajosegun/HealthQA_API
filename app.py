ba8a1ba944d7b337abd8817854b4e8c071988998
Olusegun Ajose <ajoseolusegun@gmail.com>
2023-03-08 23:11:23 +0100

    Download pkl file

--- app.py
+++ app.py
@@ -3,11 +3,44 @@ import numpy as np
 import uvicorn
 from fastapi import FastAPI, Path
 from sentence_transformers import SentenceTransformer, util
+import io
+import os
+import pickle
+import requests
 
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+encoder = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+def download_pkl_from_drive(drive_url: str, filename: str) -> None:
+    """Download a PKL file from a public Google Drive link.
+
+    Args:
+        drive_url (str): The public Google Drive URL of the file.
+        filename (str): The local file name to save the PKL file as.
+    """
+    # Get the file ID from the Google Drive URL
+    file_id = drive_url.split('/')[-2]
+    
+    # Construct the file download URL
+    download_url = f'https://drive.google.com/uc?id={file_id}'
+
+    # Download the file contents as a byte stream
+    response = requests.get(download_url)
+    content = response.content
+    
+    # Deserialize the byte stream as a PKL object
+    pkl_object = pickle.loads(content)
+    pkl_object.to_pickle(filename)
 
 link_to_dataset_on_drive = 'df_health_ds.pkl'
+
+if os.path.exists(link_to_dataset_on_drive) == False:
+    print("Attempting to download pkl file")
+    drive_url = "https://drive.google.com/file/d/1xrVoWwTaCW6AqhTxFjOvI42ZnwnhVjbJ/view?usp=sharing"
+    filename = "df_health_ds.pkl"
+    download_pkl_from_drive(drive_url, filename)
+
 df_health_ds = pd.read_pickle(link_to_dataset_on_drive)
+print("PKL file loaded")
 
 def single_answer(user_question: str, top_k: int = 1) ->list:
   user_question = user_question.strip()
@@ -15,7 +48,7 @@ def single_answer(user_question: str, top_k: int = 1) ->list:
   if user_question == '':
     return 'Please enter a valid question'
 
-  user_embeddings = model.encode(user_question, convert_to_tensor=True)
+  user_embeddings = encoder.encode(user_question, convert_to_tensor=True)
 
   #Compute cosine-similarities for each sentence with each other sentence
   questions_embeddings = df_health_ds['questions_embeddings'].tolist()
